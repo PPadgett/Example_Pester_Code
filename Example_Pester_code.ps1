8eb08a71a98cd3e579aec9389c8a9bc755ff5c9e
PPadgett <Preston.Padgett@Outlook.com>
2021-04-07 13:32:29 -0500

    Add G Drive If_Statement

--- Example_Pester_code.ps1
+++ Example_Pester_code.ps1
@@ -5,5 +5,13 @@
     else{
         Write-output "Folder Exist"
     }
+
+    # Folder on Fake G Drive
+    if (-Not (Test-Path 'G:\ExampleTestFolder') ){
+        New-Item -Path "G:\" -Name "ExampleTestFolder" -ItemType "directory"
+    }
+    else{
+        Write-output "Folder Exist"
+    }
 }
 Example_Pester_code
