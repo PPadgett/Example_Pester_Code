20a7227342e8b6bca001c85139276d69ea5d2128
PPadgett <Preston.Padgett@Outlook.com>
2021-04-07 13:24:48 -0500

    Code for testing a Real Folder & Mock Folder

---
+++ Example_Pester_code.Tests.ps1
@@ -0,0 +1,21 @@
+﻿BeforeAll {
+    . $PSCommandPath.Replace('.Tests.ps1', '.ps1')
+}
+Describe "Folder on C Drive" {
+    Context "Real Drive: C" {        
+        It "Test Folder Path on C" {
+            $FolderLocation = 'C:\ExampleTestFolder'
+            $FolderLocation | Should -Exist
+        }
+    }
+}
+
+Describe "Folder on G Drive" {
+    Context "Fake Drive: G" {        
+        It "Test Folder Path on G" {
+            $FolderLocation = 'G:\ExampleTestFolder'
+            Mock -CommandName Test-Path -MockWith { $true }
+            Test-Path -Path $FolderLocation | Should -BeTrue
+        }
+    }
+}
---
+++ Example_Pester_code.ps1
@@ -0,0 +1,9 @@
+﻿function Example_Pester_code {
+    if (-Not (Test-Path 'C:\ExampleTestFolder') ){
+        New-Item -Path "c:\" -Name "ExampleTestFolder" -ItemType "directory"
+    }
+    else{
+        Write-output "Folder Exist"
+    }
+}
+Example_Pester_code
